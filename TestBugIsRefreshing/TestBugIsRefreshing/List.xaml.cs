6a7971720a91f3834121056f586f71d08eb55809
Stefan Schrey <Stefan.Schrey@mediahuis.be>
2020-01-28 12:40:24 +0100

    First

---
+++ TestBugIsRefreshing/TestBugIsRefreshing/List.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestBugIsRefreshing
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class List : ContentPage
+    {
+        private SomeViewModel viewModel;
+        public List()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new SomeViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            viewModel.LoadItemsCommand.Execute(null);
+        }
+
+    }
+
+    public class SomeViewModel: INotifyPropertyChanged
+    {
+        public Command LoadItemsCommand { get; set; }
+
+        public SomeViewModel()
+        {
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+        }
+        async Task ExecuteLoadItemsCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            await Task.Delay(10);
+
+            IsBusy = false;
+        }
+
+        bool isBusy = false;
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set { SetProperty(ref isBusy, value); }
+        }
+
+        protected bool SetProperty<T>(
+           ref T backingStore, T value,
+           [CallerMemberName]string propertyName = "",
+           Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raises the property changed event.
+        /// </summary>
+        /// <param name="propertyName">Property name.</param>
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+
+}
---
+++ TestBugIsRefreshing/TestBugIsRefreshing/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestBugIsRefreshing
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Tab());
+            //await Navigation.PushAsync(new List());
+        }
+    }
+}
